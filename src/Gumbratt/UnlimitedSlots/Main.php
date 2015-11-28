63f7e0394f2cbdf3eb7b3e29a753ca9c9276f188
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-11-28 22:11:00 +0800

    Update Main.php

--- src/Gumbratt/UnlimitedSlots/Main.php
+++ src/Gumbratt/UnlimitedSlots/Main.php
@@ -8,12 +8,14 @@ use pocketmine\event\player\PlayerKickEvent;
 	class Main extends Plugin implements Listener {
 		public function onEnable() {
 			$this->getServer()->getPluginManager()->registerEvents($this, $this);
-			$this->getServer()->getLogger()->info("UnlimitedSlots has enabled!");
+			$this->getServer()->getLogger()->info("UnlimitedSlots has been enabled!");
 		}
 		
 		public function onPlayerKick(PlayerKickEvent $event) {
-			if($event->getReason() === "disconnectionScreen.serverFull")
+			if($event->getReason() === "disconnectionScreen.serverFull"){
 				$event->setCancelled(true);
+				$this->getLogger()->info('Server is full; forcing '.$event->getPlayer()->getName().' to join.');
+			}	
 		}
 		
 		public function onDisable() {
